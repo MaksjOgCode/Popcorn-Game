742991203f10e399a9340f4df1ca85bbf181304b
MaksjOG <maksj.og.code@gmail.com>
2024-01-25 22:23:13 +0300

    Added base funtional for animation object brick and refactoring code

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -4,9 +4,9 @@ HBRUSH Brush_PaleBlue,  Brush_Blue, Brush_WoodyBlue;
 HPEN   Brick_Pen_White_Outline, Platform_Pen_White_Outline;
 
 enum EBrickType {
-   Pale_Blue   = 101,   // RGB - (193, 217, 249);
-   Blue        = 102,   // RGB - (90, 137, 185);
-   Woody_Blue  = 103    // RGB - (46, 69, 83);
+   Pale_Blue   = 101,   /* RGB - (193, 217, 249); */
+   Blue        = 102,   /* RGB - (90, 137, 185);  */
+   Woody_Blue  = 103    /* RGB - (46, 69, 83);    */
 };
 
 char Level_01[14][12] =
@@ -29,7 +29,7 @@ char Level_01[14][12] =
 
 //---------------------------------------------------------------------------------------------------
 void Init()
-{  // Setting up the game at startup :
+{  /* Setting up the game at startup : */
 
    Brush_PaleBlue  = CreateSolidBrush(RGB(193, 217, 249));
    Brush_Blue      = CreateSolidBrush(RGB(90,  137, 185));
@@ -40,7 +40,7 @@ void Init()
 }
 //---------------------------------------------------------------------------------------------------
 void DrawBrick(HDC hdc, EBrickType color_brick, int x_offset, int y_offset)
-{  // The function of the drawning game object: Brick
+{  /* The function of the drawning game object: Brick */
 
    SelectObject(hdc, Brick_Pen_White_Outline);
    switch(color_brick) {
@@ -67,19 +67,85 @@ void DrawBrick(HDC hdc, EBrickType color_brick, int x_offset, int y_offset)
                2 * GRAPHIC_GLOBAL_SCALE, 2 * GRAPHIC_GLOBAL_SCALE );
 }
 //---------------------------------------------------------------------------------------------------
+void DrawBrickLetter(HDC hdc, int coord_x_pos, int coord_y_pos, int rotation_step)
+{  /* The function drawning animation on fall brick : */
+
+	double offset;
+	double rotation_angle = 2.0 * M_PI / 16.0 * (double)rotation_step;  /* Converting a step to a rotation angle */
+	int brick_half_height = GRAPHIC_BRICK_HEIGHT * GRAPHIC_GLOBAL_SCALE / 2;
+	int back_part_offset;
+	XFORM xform, old_xform;
+
+	if (rotation_step == 4 || rotation_step == 12)
+	{
+      /* Brick background */
+		SelectObject(hdc, Brick_Pen_White_Outline);
+		SelectObject(hdc, Brush_WoodyBlue);
+
+		Rectangle(hdc, coord_x_pos,
+                     coord_y_pos + brick_half_height - GRAPHIC_GLOBAL_SCALE,
+                     coord_x_pos + GRAPHIC_BRICK_WIDTH * GRAPHIC_GLOBAL_SCALE,
+                     coord_y_pos + brick_half_height);
+
+      /* The front of the brick : */
+		SelectObject(hdc, Brick_Pen_White_Outline);
+		SelectObject(hdc, Brush_PaleBlue);
+
+		Rectangle(hdc, coord_x_pos,
+                     coord_y_pos + brick_half_height,
+                     coord_x_pos + GRAPHIC_BRICK_WIDTH * GRAPHIC_GLOBAL_SCALE,
+                     coord_y_pos + brick_half_height + GRAPHIC_GLOBAL_SCALE - 1);
+	}
+	else
+	{
+		SetGraphicsMode(hdc, GM_ADVANCED);
+
+		/* The matrix of the letter reversal : */
+		xform.eM11 = 1.0f;
+		xform.eM12 = 0.0f;
+		xform.eM21 = 0.0f;
+		xform.eM22 = (float)cos(rotation_angle);
+		xform.eDx = (float)coord_x_pos;
+		xform.eDy = (float)coord_y_pos + (float)(brick_half_height);
+		GetWorldTransform(hdc, &old_xform);
+		SetWorldTransform(hdc, &xform);
+	
+		/* Brick background */
+		SelectObject(hdc, Brick_Pen_White_Outline);
+		SelectObject(hdc, Brush_WoodyBlue);
+
+		offset = 3.0 * (1.0 - fabs(xform.eM22)) * (double)GRAPHIC_GLOBAL_SCALE;
+		back_part_offset = (int)round(offset);
+		Rectangle(hdc, 0, -brick_half_height - back_part_offset,
+                        GRAPHIC_BRICK_WIDTH * GRAPHIC_GLOBAL_SCALE,
+                        brick_half_height - back_part_offset);
+
+      /* The front of the brick : */
+		SelectObject(hdc, Brick_Pen_White_Outline);
+		SelectObject(hdc, Brush_PaleBlue);
+
+		Rectangle(hdc, 0, -brick_half_height,
+                        GRAPHIC_BRICK_WIDTH * GRAPHIC_GLOBAL_SCALE,
+                        brick_half_height);
+
+		SetWorldTransform(hdc, &old_xform);
+   }
+
+}
+//---------------------------------------------------------------------------------------------------
 void DrawPlatform(HDC hdc, int coord_x_pos, int coord_y_pos)
-{  // The function of the drawning game object: Platform
+{  /* The function of the drawning game object: Platform */
 
    SelectObject(hdc, Brush_PaleBlue);
    SelectObject(hdc, Platform_Pen_White_Outline);
 
-   for (short i = 0, offset = 0; i < 2; ++i, offset += 21)     // Draw two cicrle (the first, and the second drawning with offset x_coord (21)) :
+   for (short i = 0, offset = 0; i < 2; ++i, offset += 21)     /* Draw two cicrle (the first, and the second drawning with offset x_coord (21)) :  */
       Ellipse( hdc, (coord_x_pos + offset) * GRAPHIC_GLOBAL_SCALE,
                      coord_y_pos * GRAPHIC_GLOBAL_SCALE,
                     (coord_x_pos + GRAPHIC_CIRCLE_SIZE + offset) * GRAPHIC_GLOBAL_SCALE,
                     (coord_y_pos + GRAPHIC_CIRCLE_SIZE) * GRAPHIC_GLOBAL_SCALE);
    
-   // Draw highlight on circle :
+   /* Draw highlight on circle : */
    Arc(hdc, (coord_x_pos + 1) * GRAPHIC_GLOBAL_SCALE,
             (coord_y_pos + 1) * GRAPHIC_GLOBAL_SCALE,
             (coord_x_pos + GRAPHIC_CIRCLE_SIZE - 1) * GRAPHIC_GLOBAL_SCALE,
@@ -99,7 +165,7 @@ void DrawPlatform(HDC hdc, int coord_x_pos, int coord_y_pos)
 }
 //---------------------------------------------------------------------------------------------------
 void DrawLevel(HDC hdc)
-{  // The function of drawning the level of game :
+{  /* The function of drawning the level of game : */
 
    for (int i = 0; i < 14; ++i) {
       for (int j = 0; j < 12; ++j) {
@@ -108,13 +174,17 @@ void DrawLevel(HDC hdc)
             GRAPHIC_LEVEL_Y_OFFSET + i * (GRAPHIC_BRICK_HEIGHT + 1)  );
       }
    }
+
+   DrawPlatform(hdc, 80, 190);
+
 }
 //---------------------------------------------------------------------------------------------------
 void DrawFrame(HDC hdc)
-{  // The function of rendering the game frame :
+{  /* The function of rendering the game frame :   */
 
-   DrawLevel(hdc);
+   //DrawLevel(hdc);
 
-   DrawPlatform(hdc, 80, 190);
+   for(int i = 0; i < 16; ++i)
+      DrawBrickLetter(hdc, 20 + i * 16 * GRAPHIC_GLOBAL_SCALE, 100, i);
 }
 //---------------------------------------------------------------------------------------------------
--- Popcorn/Engine.hpp
+++ Popcorn/Engine.hpp
@@ -1,22 +1,25 @@
 #pragma once
 
 #include <Windows.h>
+#include <cmath>
 #include "Game_Core.hpp"
 
+# define M_PI           3.14159265358979323846  /* pi */
+
 //||:-GAME GLOBAL: MULTIPLY FOR SIZE GAME OBJECTS-:||
-#define GRAPHIC_GLOBAL_SCALE     3
+# define GRAPHIC_GLOBAL_SCALE     3
 
 //||||||||||||:-WORLD TRANSFORM: OFFSET-:||||||||||||
-#define GRAPHIC_LEVEL_X_OFFSET   8
-#define GRAPHIC_LEVEL_Y_OFFSET   6
+# define GRAPHIC_LEVEL_X_OFFSET   8
+# define GRAPHIC_LEVEL_Y_OFFSET   6
 
 //||||||||||||||:-GAME OBJECT: BRICK-:|||||||||||||||
-#define GRAPHIC_BRICK_WIDTH      15
-#define GRAPHIC_BRICK_HEIGHT     7
+# define GRAPHIC_BRICK_WIDTH      15
+# define GRAPHIC_BRICK_HEIGHT     7
 
 //|||||||||||||:-GAME OBJECT: PLATFORM-:|||||||||||||
-#define GRAPHIC_CIRCLE_SIZE		7
-#define GRAPHIC_INNER_WIDTH		21
+# define GRAPHIC_CIRCLE_SIZE		7
+# define GRAPHIC_INNER_WIDTH		21
 
 enum EBrickType;
 
